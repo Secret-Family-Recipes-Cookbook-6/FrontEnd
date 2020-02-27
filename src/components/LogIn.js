330d52b2fa781f0e0d3218b9b94871a2322bca76
Alison Ludick <alison.ludick@gmail.com>
2020-02-27 09:41:27 +0200

    Added Header file, update axiosWithAuth file, updated LogIn file

---
+++ src/components/LogIn.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Header from './Header';
+import { Formik, Form, Field } from 'formik';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
