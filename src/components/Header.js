2df295fc2c89e9ee72dfcb63270bd0810cc94fec
Alison Ludick <alison.ludick@gmail.com>
2020-02-27 09:50:26 +0200

    update heading in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const Header = () => {
     return (
         <section className='header'>
-            <Link to='/'><h1>Farm Fresh</h1></Link>
+            <Link to='/'><h1>Recipe Vault</h1></Link>
         </section>
     )
 };
