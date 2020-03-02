69a0c93e6667d83f2ef34017b966042000e2cb61
Code4Blessings <robinwarden@code4blessings.com>
2020-03-01 21:09:52 -0500

    Moved Recipe code from index.js to App.js, functionality still intact

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
+import React, { useState } from 'react';
+import Recipes from "./components/Recipes";
+import RecipeForm from "./components/RecipeForm";
+import "./App.css";
 
+const App = () => {
+    const [recipes, setRecipes] = useState([
+    {
+      id: 1,
+      title: "Cheese Crackers",
+      body:
+        "2 Cups Of Assorted Cheeses, Grated Generously, Spread Over Crackers", 
+        footer: "Aunt Mary"
+    }
+  ]);
+
+  const addNewRecipe = recipe => {
+    const newRecipe = {
+      id: Date.now(), // gives a unique id
+      title: recipe.title, 
+      body: recipe.body, 
+      footer: recipe.footer
+    }; 
+    setRecipes([...recipes, newRecipe])
+  }
+
+    return (
+        <div className="App">
+            <h1>Family Recipes</h1>
+            <RecipeForm addNewRecipe={addNewRecipe} />
+            <Recipes recipes={recipes} />
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import Recipes from "./components/Recipes";
-import RecipeForm from "./components/RecipeForm";
+import "./index.css";
+import App from "./App";
 //mimic fetching data from an API
 // import data from "./data";
-import "./App.css";
 
-function App() {
- 
-  const [recipes, setRecipes] = useState([
-    {
-      id: 1,
-      title: "Cheese Crackers",
-      body:
-        "2 Cups Of Assorted Cheeses, Grated Generously, Spread Over Crackers", 
-        footer: "Aunt Mary"
-    }
-  ]);
-
-  const addNewRecipe = recipe => {
-    const newRecipe = {
-      id: Date.now(), // gives a unique id
-      title: recipe.title, 
-      body: recipe.body, 
-      footer: recipe.footer
-    }; 
-    setRecipes([...recipes, newRecipe])
-  }
-  return (
-    <div className="App">
-      <h1>Family Recipes</h1>
-      <RecipeForm addNewRecipe={addNewRecipe} />
-      <Recipes recipes={recipes} />
-    </div>
-  );
-}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, rootElement)
