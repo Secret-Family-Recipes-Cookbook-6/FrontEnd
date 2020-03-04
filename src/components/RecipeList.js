b5f38867cef8c6299f0a4634832af3c6af5a8103
Alison Ludick <alison.ludick@gmail.com>
2020-03-04 07:44:10 +0200

    updated RecipeList.js with map method

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,15 +1,20 @@
 import React from 'react'; 
 import Recipes from './Recipes'; 
 
-
 const RecipeList = () => {
     return (
         <div>
-            
+            <h2>Recipe List</h2>
+            {recipe.map(recipeList =>
+                <Recipes
+                key={recipeList.id}
+                recipe={recipeList}
+                addNewRecipe={addNewRecipe} />
+            )}
+
         </div>
     )
 }
-
 
 export default RecipeList; 
 
