663abe493119abdc8e97e9666743e74682b5cd49
Kristi Gribble <kgribble4083@gmail.com>
2020-03-04 17:31:13 -0600

    changed UpdateRecipe - not sure if we should use this.

--- src/components/UpdateRecipe.js
+++ src/components/UpdateRecipe.js
@@ -3,18 +3,20 @@ import axios from "axios";
 
 
 const initialRecipe = {
-    id: "",
     title: "",
-    body: [],
-    footer: ""
-}
+    source: "",
+    ingredients: "",
+    instructions: "",
+    image: "",
+    category: ""
+  }
 
 const UpdateRecipe = (props) => {
     const [recipes, setRecipes] = useState(initialRecipe);
 
     useEffect(() => {
         axios
-            .get(`/recipes/${props.id}`)
+            .get(`/auth/recipes/${props.id}`)
             .then(resonse => setRecipes(response.data))
     }, [props.recipes, props.id])
 
@@ -32,30 +34,64 @@ const UpdateRecipe = (props) => {
     <div>
         <form className="recipe-list" onSubmit={handleSubmit}>
             <h2>Update Recipe</h2>
-            <input 
-                type="text"
-                name="title"
-                placeholder="Recipe Title"
-                value={recipes.title}
-                onChange={handleChange}
-            />
-            <input 
-                type="text"
-                name="body"
-                placeholder="Recipe Instructions"
-                value={recipes.body}
-                onChange={handleChange}
-            />
-            <input 
-                type="text"
-                name="footer"
-                placeholder="Recipe Author"
-                value={recipes.footer}
-                onChange={handleChange}
-            />
+            <label htmlFor="title">Title</label>
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Add the recipe title"
+                    onChange={handleChange}
+                    value={recipes.title}
+                />
+     
+            <label htmlFor="source">Recipe Source</label>
+                <textarea
+                    type="text"
+                    name="source"
+                    placeholder="Who did the recipe come from"
+                    onChange={handleChange}
+                    value={recipes.source}
+               />
 
-            <button type="submit" onClick={() => {props.history.push(`update-recipe/${props.recipes.id}`)}}>Update Recipe</button>
+            <label htmlFor="ingredients">Ingredients List</label>
+                <textarea
+                    type="text"
+                    name="ingredients"
+                    placeholder="Ingredients for this recipe"
+                    onChange={handleChange}
+                    value={recipes.ingredients}
+                />
+
+            <label htmlFor="instructions">Recipe Instructions</label>
+                <textarea
+                    type="text"
+                    placeholder="Instructions for this recipe"
+                    name="instructions"
+                    onChange={handleChange}
+                    value={recipes.instructions}
+                />
+
+            <label htmlFor="image">Photo(Optional)</label>
+                <textarea
+                    type="text"
+                    name="image"
+                    placeholder="Photo of prepared recipe"
+                    onChange={handleChange}
+                    value={recipes.image}
+                />
+
+            <label htmlFor="category">Recipe Category</label>
+                <textarea
+                    type="text"
+                    name="category"
+                    placeholder="Breakfast, Lunch, Dinner or Snack?"
+                    onChange={handleChange}
+                    value={recipes.category}
+                />
+
+            <button type="submit" onClick={() => {props.history.push(`/auth/recipes/${props.recipes.id}`)}}>Update Recipe</button>
         </form>
         </div>
     )
 }
+
+export default UpdateRecipe;
