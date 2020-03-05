cb9d11e3f9c16f9aecdd88abbd58baa4c1e8f635
Kristi Gribble <kgribble4083@gmail.com>
2020-03-05 12:14:50 -0600

    Set conditional rendering of updateform

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,45 +1,81 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { secretFamilyContext } from "../context/secretFamilyContext";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import UpdateRecipe from "./UpdateRecipe";
-const Recipes = props => {
-  console.log("Props from Recipes: ", props)
-  const { recipes, setRecipes } = useContext(secretFamilyContext)
-  // const [recipe, setRecipe] = useState({
-  //   title: "",
-  //   source: "",
-  //   ingredients: "",
-  //   instructions: "",
-  //   image: "",
-  //   category: ""
-  // });
 
-  const handleDelete = (id) => {
+const initialRecipe = {
+  title: "",
+  source: "",
+  ingredients: "",
+  instructions: "",
+  image: "",
+  category: ""
+}
+
+const Recipes = ({ recipe }) => {
+  
+  const { recipes, setRecipes } = useContext(secretFamilyContext)
+  const [updateRecipe, setUpdateRecipe] = useState(initialRecipe);
+
+
+  const handleEdit = recipe => {
+    
+    if (updateRecipe.id) {
+      axiosWithAuth()
+          .put(`/auth/recipes/${updateRecipe.id}`, updateRecipe)
+          .then(response => {
+              console.log("put response: ", response)
+              setUpdateRecipe({
+                  title: "",
+                  source: "",
+                  ingredients: "",
+                  instructions: "",
+                  image: "",
+                  category: ""
+          })
+          setRecipes([...recipes.filter(recipe => recipe.id !== updateRecipe.id), updateRecipe])
+          
+      })
+          .catch(err => console.log("Error in Recipe", err))
+    } else {
+      setUpdateRecipe(recipe)
+    }
+    
+  }
+
+  const handleDelete = recipe => {
     axiosWithAuth()
-      .delete(`/auth/recipes/${id}`)
-      .then(response => setRecipes(recipes.filter(recipe => recipe.id !== id)))
+      .delete(`/auth/recipes/${recipe.id}`)
+      .then(response => {
+      setRecipes(recipes.filter(item => item.id !== recipe.id))
+      })
       
       .catch(err => console.log("Error in Delete Function: ", err))
-  }
+    }
 
   return (
     <div className="recipe-list">
-      
+      {recipe.id === updateRecipe.id ? (
         <div className="recipe">
-
-       
-          <h2>{props.recipe.title}</h2>
-          <p>{props.recipe.source}</p>
-          <p>{props.recipe.ingredients}</p>
-          <p>{props.recipe.instructions}</p>
-          <p>{props.recipe.image}</p>
-          <p>{props.recipe.category}</p>
-
-        <button className="delete-button" onClick={() => handleDelete(props.recipe.id)}>Delete this recipe</button>
-        <button className="delete-button" onClick={() => <UpdateRecipe />}>Update this recipe</button>
+          {<UpdateRecipe />}
         </div>
+      ) : (
+        <div className="recipe">
+          <h2>{recipe.title}</h2>
+          <p>{recipe.source}</p>
+          <p>{recipe.ingredients}</p>
+          <p>{recipe.instructions}</p>
+          <p>{recipe.image}</p>
+          <p>{recipe.category}</p>
 
-    </div>
+          <button className="delete-button" onClick={() => handleDelete(recipe)}>Delete this recipe</button>
+          <button className="delete-button" onClick={() => handleEdit(recipe)}>Update this recipe</button>
+        </div>
+          )} 
+        
+        
+      </div>
+      
   );
 };
 
